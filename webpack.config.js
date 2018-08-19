acccb59c0e2d8628f13920ddbd25ef59bf2154a0
Stephane Seng <seng.stephane@gmail.com>
2018-08-19 13:21:19 +0200

    chore(*): Add octicons as a yarn dependecy instead of manually copying its assets in this project.

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ Encore
     // .enableVersioning(Encore.isProduction())
 
     // uncomment to define the assets of the project
-    // .addEntry('js/app', './assets/js/app.js')
+    .addEntry('js/app', './assets/js/app.js')
     .addStyleEntry('css/app', './assets/css/app.css')
     .addStyleEntry('css/bootstrap', './node_modules/bootstrap/dist/css/bootstrap.css')
 
